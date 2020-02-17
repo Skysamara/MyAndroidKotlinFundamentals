da52df3df2345a53cc7657841b91ab08abaeb003
skysamara <skysamara@gmail.com>
2020-02-18 01:11:45 +0300

    done

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -28,17 +28,15 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.aboutme.databinding.ActivityMainBinding
 
 
-private lateinit var binding: ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Ivan Penrov")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-//        findViewById<Button>(R.id.done_button).setOnClickListener {addNickname(it)}
+        binding.myName= myName
         binding.doneButton.setOnClickListener { addNickname(it) }
     }
 
@@ -47,7 +45,8 @@ class MainActivity : AppCompatActivity() {
      */
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
